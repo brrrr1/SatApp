484dcab09bd655e70d852070975a2914c53531f9
Dorado <dorado.gumoi24@triana.salesianos.edu>
2025-01-27 13:32:19 +0100

    Anotaciones de los modelos

--- satapp/src/main/java/com/salesianos/satapp/model/Ubicacion.java
+++ satapp/src/main/java/com/salesianos/satapp/model/Ubicacion.java
@@ -1,4 +1,16 @@
 package com.salesianos.satapp.model;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class Ubicacion {
 }
--- satapp/src/main/java/com/salesianos/satapp/model/Usuario.java
+++ satapp/src/main/java/com/salesianos/satapp/model/Usuario.java
@@ -1,5 +1,17 @@
 package com.salesianos.satapp.model;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class Usuario {
 
 

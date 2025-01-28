a57b19e02bc25f810515c46a2eb325f05845dfaf
Moisés Dorado <dorado.gumoi24@triana.salesianos.edu>
2025-01-28 13:07:51 +0100

    estoy cansado jefe

---
+++ satapp/src/main/java/com/salesianos/satapp/repository/HistoricoCursosRepository.java
@@ -0,0 +1,7 @@
+package com.salesianos.satapp.repository;
+
+import com.salesianos.satapp.model.HistoricoCursos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoricoCursosRepository extends JpaRepository<HistoricoCursos, Long> {
+}

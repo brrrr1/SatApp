3025cffe887779f5c1bcd6bd498de8a27d14aa3c
br1 <delgado.hebru24@triana.salesianos.edu>
2025-01-29 14:23:56 +0100

    Controlador de incidencia hecho y documentado. Falta ir añadiendo atributos a los metodos (estado, usuario, ubicacion, etc...)

--- satapp/src/main/java/com/salesianos/satapp/controller/IncidenciaController.java
+++ satapp/src/main/java/com/salesianos/satapp/controller/IncidenciaController.java
@@ -1,13 +1,22 @@
 package com.salesianos.satapp.controller;
 
+import com.salesianos.satapp.dto.CreateIncidenciaDto;
 import com.salesianos.satapp.dto.GetIncidenciaDto;
+import com.salesianos.satapp.model.Estado;
 import com.salesianos.satapp.model.Incidencia;
 import com.salesianos.satapp.service.IncidenciaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +28,151 @@ public class IncidenciaController {
 
     private final IncidenciaService incidenciaService;
 
+    @Operation(summary = "Obtiene todas las bicicletas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se han encontrado incidencias",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = GetIncidenciaDto.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                                  {
+                                                      "id": 1,
+                                                      "fecha": "29/01",
+                                                      "titulo": "Aire acondicionado 2DAM",
+                                                      "descripcion": "El aire acondicionado en la clase de 2DAM no va desde Octubre. Además, no hay mando.",
+                                                      "estado": "Abierta",
+                                                      "urgencia": "Muy urgente"
+                                                      
+                                                  },
+                                                  {
+                                                      "id": 2,
+                                                      "fecha": "30/01",
+                                                      "titulo": "Proyector 1AyF",
+                                                      "descripcion": "El proyector en la clase de 1AyF ha dejado de funcionar.",
+                                                      "estado": "Abierta",
+                                                      "urgencia": "Para YA"
+                                                      
+                                                  }
+                                            ]
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado ninguna incidencia",
+                    content = @Content),
+    })
     @GetMapping
     public List<GetIncidenciaDto> getIncidencias() {
         return incidenciaService.findAll().stream().map(GetIncidenciaDto::of).toList();
     }
+
+    @Operation(summary = "Obtiene una incidencia por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha encontrado la incidencia",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = GetIncidenciaDto.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                      "id": 1,
+                                                      "fecha": "29/01",
+                                                      "titulo": "Aire acondicionado 2DAM",
+                                                      "descripcion": "El aire acondicionado en la clase de 2DAM no va desde Octubre. Además, no hay mando.",
+                                                      "estado": "Abierta",
+                                                      "urgencia": "Muy urgente"
+                                                      
+                                            }
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado la incidencia",
+                    content = @Content),
+    })
+    @GetMapping("/{id}")
+    public GetIncidenciaDto getIncidencia(@PathVariable Long id) {
+        return GetIncidenciaDto.of(incidenciaService.findById(id));
+    }
+
+    @Operation(summary = "Crea una incidencia")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha creado la incidencia",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = GetIncidenciaDto.class)),
+                            examples = {@ExampleObject(
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha creado la incidencia",
+                    content = @Content),
+    })
+    @PostMapping
+    public ResponseEntity<Incidencia> create (@RequestBody CreateIncidenciaDto incidencia) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(incidenciaService.save(incidencia));
+    }
+
+    @Operation(summary = "Edita una incidencia")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha editado la incidencia",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Incidencia.class)),
+                            examples = {@ExampleObject(
+
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado ninguna incidencia",
+                    content = @Content),
+    })
+    @PutMapping("/{id}")
+    public Incidencia editUser(@RequestBody Incidencia incidencia, @PathVariable Long id) {
+        return incidenciaService.editIncidenciaUser(incidencia, id);
+    }
+
+    @Operation(summary = "Edita el estado de una incidencia")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha editado el estado de la incidencia",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Incidencia.class)),
+                            examples = {@ExampleObject(
+
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado ninguna incidencia",
+                    content = @Content),
+    })
+    @PutMapping("/{id}/estado/")
+    public Incidencia cambiarEstado(@PathVariable Long id, Estado estado) {
+        return incidenciaService.cambiarEstado(id, estado);
+    }
+
+
+    @Operation(summary = "Borra una incidencia")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha eliminado la incidencia",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Incidencia.class)),
+                            examples = {@ExampleObject(
+
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha podido borrar la incidencia",
+                    content = @Content),
+    })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteIncidencia(@PathVariable Long id) {
+        incidenciaService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- satapp/src/main/java/com/salesianos/satapp/dto/CreateIncidenciaDto.java
+++ satapp/src/main/java/com/salesianos/satapp/dto/CreateIncidenciaDto.java
@@ -8,14 +8,15 @@ public record CreateIncidenciaDto(
         LocalDateTime fecha,
         String titulo,
         String descripcion,
-        Estado estado,
-        Usuario usuario,
+        /*Estado estado,*/
+        String urgencia
+        /*Usuario usuario,
         Categoria categoria,
-        Ubicacion ubicacion
+        Ubicacion ubicacion*/
 ) {
 
     public static CreateIncidenciaDto to(Incidencia incidencia) {
-        return new CreateIncidenciaDto(incidencia.getFecha(), incidencia.getTitulo(), incidencia.getDescripcion(), incidencia.getEstado(), incidencia.getUsuario(), incidencia.getCategoria(), incidencia.getUbicacion());
+        return new CreateIncidenciaDto(incidencia.getFecha(), incidencia.getTitulo(), incidencia.getDescripcion(), /*incidencia.getEstado(),*/ incidencia.getUrgencia()/*, incidencia.getUsuario(), incidencia.getCategoria(), incidencia.getUbicacion()*/);
     }
 
 }

b932138281af62e5b5728f057dd2ab3a85f5e793
bruno <brunodelher@gmail.com>
2025-02-10 13:40:30 +0100

    Añadidas restricciones para métodos según el rol

--- satapp/src/main/java/com/salesianos/satapp/security/SecurityConfig.java
+++ satapp/src/main/java/com/salesianos/satapp/security/SecurityConfig.java
@@ -19,17 +19,22 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
 
-        UserDetails user = User.withUsername("user")
+        UserDetails alumno = User.withUsername("alumno")
                 .password("{noop}1234")
-                .roles("USER")
+                .roles("ALUMNO")
                 .build();
 
-        UserDetails admin = User.withUsername("admin")
+        UserDetails tecnico = User.withUsername("tecnico")
                 .password("{noop}admin")
-                .roles("ADMIN")
+                .roles("TECNICO")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        UserDetails profesor = User.withUsername("profesor")
+                .password("{noop}profesor")
+                .roles("PROFESOR")
+                .build();
+
+        return new InMemoryUserDetailsManager(alumno, tecnico, profesor);
     }
 
     @Bean
@@ -38,7 +43,15 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers(HttpMethod.POST, "/task/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/tecnico/**").hasRole("TECNICO")
+                .requestMatchers(HttpMethod.GET, "/incidencia/**").hasRole("TECNICO")
+                .requestMatchers(HttpMethod.POST, "/incidencia/**").hasRole("ALUMNO")
+                .requestMatchers(HttpMethod.PUT, "/incidencia/**").hasRole("TECNICO")
+                .requestMatchers(HttpMethod.GET, "/tecnico/**").hasRole("TECNICO")
+                .requestMatchers(HttpMethod.POST, "/profesor/**").hasRole("PROFESOR")
+                .requestMatchers(HttpMethod.GET, "/profesor/**").hasRole("PROFESOR")
+                .requestMatchers(HttpMethod.POST, "/alumno/**").hasRole("ALUMNO")
+                .requestMatchers(HttpMethod.GET, "/alumno/**").hasRole("ALUMNO")
                 .anyRequest().authenticated()
         );
 
